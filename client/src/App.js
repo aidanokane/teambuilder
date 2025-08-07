1e0984639add9952fb3467e808b162e84de577de
Aidan Kane <aok26@drexel.edu>
2025-08-06 20:27:30 -0400

    start login popup

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,76 @@
 import { useEffect, useState } from "react";
 import './App.css';
 
+function closePopup(setMessage){
+  setMessage(false);
+  sessionStorage.setItem("message", "false");
+}
+
+function signIn(username, password){
+    
+}
+
+function Popup(setMessage){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    
+    return (
+          <div className="modal-overlay">
+            <div className="modal">
+              <h2>Get Started</h2>
+              
+              <input
+              type="text"
+              label="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              />
+
+              <input
+              type="text"
+              label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button>Create Account</button>
+              <button>Sign In</button>
+              <button onClick={() => {closePopup(setMessage)}}>Continue Without</button>
+            </div>
+          </div>
+        )
+}
+
 function App() {
-  const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    fetch("/test")
-      .then((res) => res.text())
-      .then((data) => setMessage(data));
-  }, []);
+    const [message, setMessage] = useState(() => {
+        return sessionStorage.getItem("message") !== "false";
+    });
+    // useEffect(() => {
+    //     fetch("/test")
+    //     .then((res) => res.text())
+    //     .then((data) => setMessage(data));
+    // }, []);
 
-  useEffect(() => {
-    fetch("/test-db")
-    .then((res) => res.text())
-    .then((data) => setMessage(data));
-  });
+    // useEffect(() => {
+    // fetch("/test-db")
+    // .then((res) => res.text())
+    // .then((data) => setMessage(data));
+    // });
 
-  return (<h1>{message}</h1>);
+    return (
+      <div className="App">
+        <h1>Welcome to Pokémon Teambuilder</h1>
+        {message && <Popup setMessage={setMessage} />}
+
+<button onClick={() => {
+  sessionStorage.removeItem("message");
+  window.location.reload();
+}}>
+  Reset Session
+</button>
+      </div>
+  );
 }
 
 
